89a43e526c828885b126b345edf85b1c4d8b1106
hhshiblu <hhshiblu5555@gmail.com>
2025-04-17 21:46:52 -0700

    jkjk

--- db/db.js
+++ db/db.js
@@ -1,11 +1,9 @@
 import { MongoClient } from "mongodb"
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local")
-}
 
-const uri = process.env.MONGODB_URI
-console.log(uri)
+
+const uri = "mongodb+srv://hh_shiblu:jseshiblu55@cluster0.7np6gn8.mongodb.net/?retryWrites=true&w=majority"
+
 
 const options = {}
 
